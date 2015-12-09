9fa4c1df63bfff82074b8607c206961828e1f36a
Mo Kouli <mkouli@uber.com>
2015-12-09 07:23:56 -0800

    add twitter colors + downgrade icon size

--- app/src/main/java/com/codepath/apps/motwitter/activities/HomeActivity.java
+++ app/src/main/java/com/codepath/apps/motwitter/activities/HomeActivity.java
@@ -18,7 +18,7 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabCreate);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
